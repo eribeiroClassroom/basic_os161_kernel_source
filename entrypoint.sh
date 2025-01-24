639d6f854ee81bc76f8f1163e13d18c1d4e37ac1
Eraldo Ribeiro <eribeiro@fit.edu>
2025-01-23 21:47:26 -0500

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,28 +17,28 @@ cd /github/workspace
 cd /github/workspace
 /root/os161/tools/bin/bmake install
 
-# Build OS/161 (Userland)
-cd /github/workspace
-/root/os161/tools/bin/bmake includes
-cd /github/workspace
-/root/os161/tools/bin/bmake
-cd /github/workspace
-/root/os161/tools/bin/bmake install
+# # Build OS/161 (Userland)
+# cd /github/workspace
+# /root/os161/tools/bin/bmake includes
+# cd /github/workspace
+# /root/os161/tools/bin/bmake
+# cd /github/workspace
+# /root/os161/tools/bin/bmake install
 
-# Run the OS/161 kernel and save the output to a file
-cd /root/os161/root/
-/root/os161/tools/bin/sys161 kernel q > output.txt 
-cat output.txt
+# # Run the OS/161 kernel and save the output to a file
+# cd /root/os161/root/
+# /root/os161/tools/bin/sys161 kernel q > output.txt 
+# cat output.txt
 
-the_string=$(grep "DUMBVM" "output.txt")
-echo $the_string | grep -oP '.*(?=system version)'
-var=$(echo $the_string | grep -oP '.*(?=system version)')
+# the_string=$(grep "DUMBVM" "output.txt")
+# echo $the_string | grep -oP '.*(?=system version)'
+# var=$(echo $the_string | grep -oP '.*(?=system version)')
 
-if [ "$var" != "Put-your-group-name-here's" ]
-then
-    echo "No changes in line containing Put-your-group-name-here's"
-    exit 1
-fi
+# if [ "$var" != "Put-your-group-name-here's" ]
+# then
+#     echo "No changes in line containing Put-your-group-name-here's"
+#     exit 1
+# fi
 
 
 
